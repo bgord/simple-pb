bef39a67f85dc1dcd5e5bbba0a9c059e878f7906
Bartosz Gordon <bartosz.gordon@sealcode.org>
2018-01-09 13:47:53 +0100

    Refactor char repeating

--- index.js
+++ index.js
@@ -25,8 +25,8 @@ module.exports = class ProgressBar {
 		);
 		const empty_bar_length = this.bar_length - filled_bar_length;
 
-		const filled_bar = this.get_bar(filled_bar_length, " ", bgWhite);
-		const empty_bar = this.get_bar(empty_bar_length, "-");
+		const filled_bar = bgWhite(" ".repeat(filled_bar_length));
+		const empty_bar = "-".repeat(empty_bar_length);
 		const percentage_progress = (current_progress * 100).toFixed(2);
 
 		process.stdout.clearLine();
@@ -34,13 +34,5 @@ module.exports = class ProgressBar {
 		process.stdout.write(
 			`Current progress: [${filled_bar}${empty_bar}] | ${percentage_progress}%`
 		);
-	}
-
-	get_bar(length, char, color = a => a) {
-		let str = "";
-		for (let i = 0; i < length; i++) {
-			str += char;
-		}
-		return color(str);
 	}
 };
